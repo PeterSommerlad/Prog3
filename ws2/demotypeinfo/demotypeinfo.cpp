54e4d99e54ca5319bd8c9128010fe7c1dc2c4b99
Peter Sommerlad <peter.sommerlad@hsr.ch>
2012-09-25 11:31:50 +0200

    added values from slide with perfect forwarding

--- ws2/demotypeinfo/demotypeinfo.cpp
+++ ws2/demotypeinfo/demotypeinfo.cpp
@@ -5,14 +5,30 @@
 
 // no need to understand that now. Gnu compiler specific
 template <typename T>
-void showTypeInfoAndValue(T x){
+void showTypeInfoAndValue(T&& x){
 	char *toBeFreed = abi::__cxa_demangle(typeid(T).name(),0,0,0);
 	std::string result{toBeFreed};
 	::free(toBeFreed);
 	std::cout << x << " is of type "<<result<<'\n';
 }
-
+template <typename ...ALL>
+void showTypeInfo(ALL&&... ){}
+template <typename T, typename ...ALL>
+void showTypeInfo(T &&x,ALL&&...  rest){
+	showTypeInfoAndValue(x);
+	showTypeInfo(rest...);
+}
 int main(){
+	showTypeInfo(
+			'a', '\n', '\x0a',
+			1, 42L, 5LL,
+			1u, 42ul, 5ull,
+			020, 0x1f, 0XFULL,
+			/*0f is a syntax error */0.f, .33, 1e9,
+			42.E-12L, .3l,
+			"hello", "\012\n\\",
+			R"(\root.hsr\skripte\)");
+std::cout << "------\n";
 	showTypeInfoAndValue('a');
 	showTypeInfoAndValue(42L);
 	showTypeInfoAndValue(0xFULL);
